c8f1293a5d7852001ec706e527ba0e4bce9556ce
Leo Kettmeir <crowlkats@toaxl.com>
2025-11-16 14:51:34 +0900

    fix TimelineIndicator asset handling

--- 2025/jsconfjp/components/TimelineIndicator.vue
+++ 2025/jsconfjp/components/TimelineIndicator.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="absolute bottom-5px right-160px left-50px">
-    <img :src="`/timeline_indicator_${version}.svg`" :alt="`version 2.${version} timeline indicator`" />
+    <img :src="asset" :alt="`version 2.${version} timeline indicator`" />
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import { computed } from "vue";
+import { resolveAssetUrl } from "../layoutHelper";
+
+const props = defineProps<{
   version: string;
 }>();
+
+const asset = computed(() =>
+    resolveAssetUrl(`/timeline_indicator_${props.version}.svg`)
+);
 </script>
