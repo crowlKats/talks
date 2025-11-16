9f2761404fc532b14d5269f96bf23c558a783812
Leo Kettmeir <crowlkats@toaxl.com>
2025-11-16 14:04:13 +0900

    fix

--- 2025/jsconfjp/components/TimelineIndicator.vue
+++ 2025/jsconfjp/components/TimelineIndicator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute bottom-5px right-160px left-50px">
-    <img :src="`/timeline_indicator_${version}.svg`" />
+    <img :src="`/timeline_indicator_${version}.svg`" :alt="`version 2.${version} timeline indicator`" />
   </div>
 </template>
 
